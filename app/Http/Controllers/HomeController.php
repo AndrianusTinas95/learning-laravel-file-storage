d09462f03e2a291ee595b0cbb30ab611b6945763
andriTinas <andrianustinas.siskom@gmail.com>
2019-09-17 15:44:01 +0700

    3. Mendapatkan Informasi File yang diunggah Laravel File Storage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,14 @@ class HomeController extends Controller
     public function upload(Request $request){
     //    $path = $request->file('image')->store('public');
     //    $path = $request->file('image')->store('photo');
-        $path = Storage::putFile('public',$request->file('image'));
+        // $path = Storage::putFile('public',$request->file('image'));
+        // $path = $request->file('image')->storeAs('public','gambar');
+        $file = $request->file('image');
+        $name = time();
+        $extension = $file->getClientOriginalExtension();
+        $newName = $name . '.' . $extension;
+        // $path = $request->file('image')->storeAs('public',$newName);
+        $path = Storage::putFileAs('photo',$request->file('image'),$newName);
         dd($path);
     }
 }

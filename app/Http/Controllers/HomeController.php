7e5dac39b1d1408c52f67f12531c29400e305b80
andriTinas <andrianustinas.siskom@gmail.com>
2019-09-17 15:32:43 +0700

    2. Menggunakan Facade Storage untuk upload File di Laravel Tutorial

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends Controller
 {
     public function upload(Request $request){
-       $path = $request->file('image')->store('');
+    //    $path = $request->file('image')->store('public');
+    //    $path = $request->file('image')->store('photo');
+        $path = Storage::putFile('public',$request->file('image'));
         dd($path);
     }
 }
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ Here I am following tutorial from IDStack on youtube ,in plyalist Laravel File S
 
 here I am Learn 
 1. Cara Upload File di Laravel File Storage Tutorial
+2. Menggunakan Facade Storage untuk upload File di Laravel Tutorial
